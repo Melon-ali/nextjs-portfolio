f84081c96fb3bff13bfeb250296bdbbbbb6c5c92
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-02-14 11:33:12 +0600

    all dependencies added

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,14 +1,14 @@
 import Hero from '../../components/home/Hero'
 
 import Work from '../../components/home/work'
-import About from './about/About'
+import About from '../../components/home/About'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Hero />
-      <Work />
       <About />
+      <Work />
     </div>
   )
 }
